27854d4d7abaafcdfdaa35a03b97cc3419f21ae9
Mikael Sundberg <mikael.sundberg82@gmail.com>
2011-01-04 20:36:19 +0100

    initial version

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject FeedTutorial2 "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [enlive  "1.0.0-SNAPSHOT"]
+                 [net.cgrand/moustache "1.0.0-SNAPSHOT"]
+                 [ring/ring-core "0.2.5"]
+                 [ring/ring-devel "0.2.5"]
+                 [ring/ring-jetty-adapter "0.2.5"]])
+
+

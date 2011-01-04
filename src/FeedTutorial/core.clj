6c86440edf87e015382ea264b1fd1725cb5e2829
Mikael Sundberg <mikael.sundberg82@gmail.com>
2011-01-04 20:39:06 +0100

    first version done

--- src/FeedTutorial/core.clj
+++ src/FeedTutorial/core.clj
@@ -1,33 +1,38 @@
-
 (ns FeedTutorial.core
   ( :use net.cgrand.moustache
          net.cgrand.enlive-html
          ring.util.response
+         ring.middleware.params
          [ring.adapter.jetty :only [run-jetty]]))
 
 
 
-(def bookmarks [ {:title "Something happend!" :url "www.someurl.com"}
-                  {:title "More news." :url "www.newssite.com"}
-                  {:title "Very cool stuff" :url "www.verycoolsite.com"}])
+(def bookmarks (agent [{:title "Something happend!" :url "www.someurl.com"}
+                       {:title "More news." :url "www.newssite.com"}
+                       {:title "Very cool stuff" :url "www.verycoolsite.com"}]))
+
+
 
 
 (deftemplate index "FeedTutorial/index.html"
   [items]
-  [:div#bookmarks] (content "I changed the html")
+  [:div#bookmarks :*] (content "My own bookmarkingpage!")
   [:table#items :tr.bookmark] (clone-for [item items]
                                 [:td.title] (content (:title item))
                                 [:td.url] (content (:url item))))
+
+(defn add-bookmark [{params :params}]
+  (send bookmarks conj {:title (params "title") :url (params "url")})
+  (response (str params)))
 
 
 
 (def my-app-handler
      (app
-      [""] (-> (index bookmarks) response constantly)
+            wrap-params
+      ["addBookmark"] add-bookmark
+      [""] (-> (index @bookmarks) response constantly)
       ["hello"] (-> "My own page!" response constantly)))
-
-
-
 
 (run-jetty #'my-app-handler {:port 8080
                                   :join? false})
